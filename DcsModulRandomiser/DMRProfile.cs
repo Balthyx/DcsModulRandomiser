4370df42c3253824d57bab7dc4f12e7b4edb7f30
Balthyx <balthyx@hotmail.fr>
2021-08-31 00:32:51 +0200

    Go json go json go go go !

---
+++ DcsModulRandomiser/DMRProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DcsModulRandomiser
+{
+    public class DMRProfile
+    {
+        public int dayMin;
+        public int dayMax;
+        public string currentRollName;
+        public DateTime currentRollDate;
+
+        public List<Map> maps;
+    }
+
+    public class Map
+    {
+        public string mapName;
+        public List<Module> planeSet;
+    }
+
+    public class Module
+    {
+        public string name;
+        public List<Module> childs;
+        public bool IsALeave { get => childs.Count == 0; }
+    }
+}
--- DcsModulRandomiser/Program.cs
+++ DcsModulRandomiser/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Xml;
+using System.Text.Json;
+using System.IO;
 
 namespace DcsModulRandomiser
 {
@@ -9,13 +10,16 @@ namespace DcsModulRandomiser
         static void Main(string[] args)// arg0 doc, then reroll, forcemap mapname
         {
             Random random = new Random();
-            XmlDocument doc = new XmlDocument();
+            
+            DMRProfile dMRProfile;
             bool reroll = false;
             string forceMap = null;
             try
             {
-                doc.Load(args[0]);
-                if(args.Length >= 2)
+                string jsonString = File.ReadAllText(args[0]);
+                dMRProfile = JsonSerializer.Deserialize<DMRProfile>(jsonString);
+
+                if (args.Length >= 2)
                 {
                     for(int i=1; i<args.Length; i++)
                     {
@@ -59,19 +63,17 @@ namespace DcsModulRandomiser
 
             bool IsDateExpired()
             {
-                XmlNode currentRollNode = doc.SelectSingleNode("/ Root / CurrentRoll");
-                if(currentRollNode.Attributes.GetNamedItem("date").Value == "")
+                if(dMRProfile.currentRollDate == null)
                 {
                     return true;
                 }
-                DateTime date = DateTime.Parse(currentRollNode.Attributes.GetNamedItem("date").Value);
-                return DateTime.Today > date;
+                return DateTime.Today > dMRProfile.currentRollDate;
             }
 
             string getRandomModule(string forcedMap)
             {
                 Console.WriteLine("Picking a random module");
-                XmlNode map;
+                Map map;
                 if (forcedMap != null)
                 {
                     map = GetMapByName(forcedMap);
@@ -86,49 +88,48 @@ namespace DcsModulRandomiser
                     map = GetRandMap();
                 }
 
-                XmlNode module = RecGetRandNode(map);
+                Module module = GetRandNodeInMap(map);
 
-                string currentRoll = map.Attributes.GetNamedItem("name").Value.ToString() + " : " + module.Attributes.GetNamedItem("name").Value.ToString();
 
                 //Set CurrentRoll Node
-                XmlNode CurrentRollNode = doc.SelectSingleNode("/ Root / CurrentRoll");
-
-                CurrentRollNode.Attributes.GetNamedItem("name").Value = currentRoll;
+                dMRProfile.currentRollName = module.name;
 
                 //Set a random date
-                XmlNode ParametersNode = doc.SelectSingleNode("/ Root / Parameters");
-                int dayMin = int.Parse(ParametersNode.Attributes.GetNamedItem("dayMin").Value);
-                int dayMax = int.Parse(ParametersNode.Attributes.GetNamedItem("dayMax").Value);
 
                 DateTime rdmDate = DateTime.Today;
-                int rdm = (random.Next(dayMin, dayMax));
-                rdmDate = rdmDate.AddDays(rdm);
+                int rdm = (random.Next(dMRProfile.dayMin, dMRProfile.dayMax));
+                dMRProfile.currentRollDate = rdmDate.AddDays(rdm);
 
-                CurrentRollNode.Attributes.GetNamedItem("date").Value = rdmDate.ToShortDateString();
-                doc.Save(args[0]);
+                //Save
+                Serialize(args[0]);
 
-                return currentRoll;
+                return dMRProfile.currentRollName;
+            }
+
+            void Serialize(string Path)
+            {
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                string jsonString = JsonSerializer.Serialize(dMRProfile, options);
+                File.WriteAllText(Path, jsonString);
             }
 
             string getCurrentModule()
             {
                 Console.WriteLine("Picking last module");
-                return doc.SelectSingleNode("/ Root / CurrentRoll").Attributes.GetNamedItem("name").Value.ToString();
+                return dMRProfile.currentRollName;
             }
 
-            XmlNode GetRandMap()
+            Map GetRandMap()
             {
-                XmlNode mapNode = doc.SelectSingleNode("/ Root / List");
-                int rdm = (random.Next(0, mapNode.ChildNodes.Count));
-                return mapNode.ChildNodes.Item(rdm);
+                int rdm = (random.Next(0, dMRProfile.maps.Count));
+                return dMRProfile.maps[rdm];
             }
 
-            XmlNode GetMapByName(string mapName)
+            Map GetMapByName(string mapName)
             {
-                XmlNode mapNode = doc.SelectSingleNode("/ Root / List");
-                foreach (XmlNode chMap in mapNode)
+                foreach (Map chMap in dMRProfile.maps)
                 {
-                    if (chMap.Attributes.GetNamedItem("name").Value == mapName)
+                    if (chMap.mapName == mapName)
                     {
                         return chMap;
                     }
@@ -136,38 +137,22 @@ namespace DcsModulRandomiser
                 return null;
             }
 
-            XmlNode RecGetRandNode(XmlNode node)
+            Module RecGetRandNode(Module node)
             {
-                if (node.HasChildNodes)
-                {
-                    List<XmlNode> weightedCount = new List<XmlNode>();
-                    foreach(XmlNode mapNode in node.ChildNodes)
-                    {
-                        if(mapNode.Attributes.GetNamedItem("weight") != null)
-                        {
-                            for (int i = 0; i < int.Parse(mapNode.Attributes.GetNamedItem("weight").Value); i++)
-                            {
-                                weightedCount.Add(mapNode);
-                            }
-                        }
-                        else
-                        {
-                            weightedCount.Add(mapNode);
-                        }
-                        
-                    }
-
-                    int rdm = random.Next(0, weightedCount.Count);
-                    return RecGetRandNode(weightedCount[rdm]);
-                }
-                else
+                if (node.IsALeave)
                 {
                     return node;
                 }
+
+                int rdm = random.Next(0, node.childs.Count);
+                return RecGetRandNode(node.childs[rdm]);
             }
 
+            Module GetRandNodeInMap(Map map)
+            {
+                int rdm = random.Next(0, map.planeSet.Count);
+                return RecGetRandNode(map.planeSet[rdm]);
+            }
         }
-
-        
     }
 }
