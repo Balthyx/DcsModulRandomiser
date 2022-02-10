26e06be9230a8434c0084a116c469a11f68196af
Balthyx <balthyx@hotmail.fr>
2022-02-10 19:03:09 +0100

    Update DMRProfile.cs

--- DCSModuleRandomiser/DMRProfile.cs
+++ DCSModuleRandomiser/DMRProfile.cs
@@ -9,22 +9,24 @@ namespace DCSModulRandomiser
         public string currentRollName;
         public string currentRollDate;
 
-        public List<Map> Maps;
-    }
-
-    public class Map
-    {
-        public string mapName;
-        public List<Module> planeSet;
+        public List<Module> Modules;
     }
 
     public class Module
     {
         public string name;
         public float time_multiplier = 1;
-        public List<Module> Childs;
-        public bool IsALeave() {
-            return Childs == null || Childs.Count == 0; 
-        }
+        public float weight = 1;
+
+        public float[] periodWeights = new float[5] { 1, 1, 1, 1, 1 };
+    }
+
+    public enum TimePeriod
+    {
+        WW2,
+        Corea,
+        ColdWar60,
+        ColdWar80,
+        Modern
     }
 }
